d2abbb0f49c898f8ae6122a49f6430fa363afacc
savanahamood <savanahamood@gmail.com>
2023-06-05 17:27:49 +0000

    error handlers

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 'use strict';
 const express = require("express");
 const app = express();
+const stamper = require('./middlewares/stamper');
+const notFoundHandler=require('./handlers/404');
+const errorHandler=require('./handlers/500');
 
 
 app.get('/', (req, res) => {
     res.send("hello everyone");
 });
 
-app.get('/data',stamper, (req, res) => {
+app.get('/data', stamper, (req, res) => {
     res.json({
         id: 1,
         name: "Savana",
@@ -17,10 +20,9 @@ app.get('/data',stamper, (req, res) => {
 })
 
 
-function stamper(req,res,next){
-    req.timestamp= new Date();
-    next();
-}
+app.use('*', notFoundHandler);
+app.use(errorHandler);
+
 function start(port) {
     app.listen(port, () => {
         console.log(`server is listen on ${port}`)
